b93c761fa77389cdc76b87b1a92287bfb52c939c
Agrant9 <96260635+Agrant9@users.noreply.github.com>
2023-04-10 21:44:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 544835691 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, pval = st.ttest_ind(x, y, equal_var = False)
+    return True if pval < 0.07 else False # Ваш ответ, True или False
