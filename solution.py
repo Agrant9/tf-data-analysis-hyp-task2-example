e2f98be10c6b05797ed2fa1d4264f42a5aab53b3
Agrant9 <96260635+Agrant9@users.noreply.github.com>
2023-04-12 22:51:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,6 +9,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    pval = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+    pval = st.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
                             vectorized=True, n_resamples=1000, alternative="two-sided").pvalue
     return True if pval < 0.07 else False # Ваш ответ, True или False
